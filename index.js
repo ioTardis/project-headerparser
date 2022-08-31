1b239cf0e2bd7768bf5e658dcf86240ef21d0160
Izabella Golovina <ia.golovina00@gmail.com>
2022-08-31 20:55:21 +0200

    edit index.js for unused variables edit style.css and index.html for website appearance

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
-// index.js
-// where your node app starts
-
-// init project
 require('dotenv').config();
 const express = require('express');
 const app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-const cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
+// serving static file
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+// basic routing
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
+// API endpoint
 app.get('/api/hello', (req, res) => {
   res.json({ greeting: 'hello API' });
 });
 
-// header API
+// headerparser API
 app.get('/api/whoami', (req, res) => {
   const ipaddress = req.ip;
   const language = req.headers['accept-language'];
